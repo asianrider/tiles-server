7fbb12aa18b2606b59aa45f29bbc02ee0bbe4822
laurent <asianrider@gmail.com>
2023-01-23 19:08:50 +0100

    Initial

---
+++ index.js
@@ -0,0 +1,63 @@
+var express = require("express"),
+    app = express(),
+    MBTiles = require('@mapbox/mbtiles'),
+    fs = require('fs'),
+    path = require("path");
+
+// Enable CORS and set correct mime type/content encoding
+var header = {
+  "Access-Control-Allow-Origin":"*",
+  "Access-Control-Allow-Headers":"Origin, X-Requested-With, Content-Type, Accept",
+  "Content-Type":"application/x-protobuf",
+  "Content-Encoding":"gzip"
+};
+
+const mbtilesMap = {};
+
+// Route which handles requests like the following: /<mbtiles-name>/0/1/2.pbf
+app.get('/:source/:z/:x/:y.pbf', function(req, res) {
+  var mbtiles = mbtilesMap[req.params.source];
+  if (mbtiles != null) {
+    mbtiles.getTile(req.params.z, req.params.x, req.params.y, function(err, tile, headers) {
+      if (err) {
+        res.set({"Content-Type": "text/plain"});
+        res.status(404).send('Tile rendering error: ' + err + '\n');
+      } else {
+        res.set(header);
+        res.send(tile);
+      }
+    });
+  } else {
+    console.log("error, no source named %s", mbtiles);
+    res.set({"Content-Type": "text/plain"});
+    res.status(404).send('No source named: "' + req.params.source + '"\n');
+  }
+});
+
+//passsing directoryPath and callback function
+fs.readdir(__dirname, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        var extensionStart = file.indexOf('.mbtiles');
+        if (extensionStart != -1) {
+          new MBTiles(path.join(__dirname, file), function(err, mbtiles) {
+            if (err != null) {
+              console.log("Unable to open %s: %s", file, err);
+            } else {
+              var prefix = file.substring(0, extensionStart);
+              mbtilesMap[prefix] = mbtiles;
+              console.log("Using %s as %s", file, prefix);
+            }
+        });
+      }
+    });
+});
+
+// Starts up the server on port 3000
+console.log('Listening on port: ' + 3000);
+app.listen(3000);
